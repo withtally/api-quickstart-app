248bd9c982f0c17446c38531c96979c599dad553
Nicolas Tourne <nicotourne@gmail.com>
2024-09-26 15:06:02 -0300

    feat(poc): Update link

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://api-quickstart-2i87ar3ls.preview.tally.xyz/",
+  base: "https://api-quickstart-app-git-feat-poc-connect-wallet.preview.tally.xyz/",
 });
